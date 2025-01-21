9ee9aaec05def8f730b82d97a60498c3a1d28f70
just-stv <22718607+just-stv@users.noreply.github.com>
2025-01-20 16:27:44 -0800

    fix: new line

--- go/transaction_information/types.go
+++ go/transaction_information/types.go
@@ -3,10 +3,10 @@ package transaction_information
 import "github.com/soralabs/solana-toolkit/go/internal/tx_parser"
 
 type TransactionInformationInput struct {
-    Hash string `json:"hash"`
+	Hash string `json:"hash"`
 }
 
 type TransactionInformationOutput struct {
-    Hash  string                `json:"hash"`
-    Swaps []*tx_parser.SwapInfo `json:"swaps"`
+	Hash  string                `json:"hash"`
+	Swaps []*tx_parser.SwapInfo `json:"swaps"`
 }
